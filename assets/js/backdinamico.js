35122e0c5cb7f7020b591643ec264f98267bf37c
LeandroDevLab <prof.oliota@gmail.com>
2025-06-02 21:31:26 -0300

    refactor: bg dinamico com Deivid Lahasse in live

--- assets/js/backdinamico.js
+++ assets/js/backdinamico.js
@@ -1,23 +1,15 @@
 // IIFE - Immediately Invoked Function Expression (protegendo o escopo da função)
-(function () {
-    
+(function () { 
     const tempo = document.querySelectorAll('.tempo');
-
+    /* refatorado com Deivid Lahasse */
     tempo.forEach((classes) => {
         classes.addEventListener('mouseenter', () => {
-            if (classes.classList.contains('frio')) {
-                return document.body.style.background = "url(../assets/img/frio.png)";
-            };
-            if (classes.classList.contains('sol')) {
-                return document.body.style.background = 'orange';
-            };
-            if (classes.classList.contains('chuva')) {
-                return document.body.style.background = 'cyan';
-            };
-            if (classes.classList.contains('nublado')) {
-                return document.body.style.background = 'gray';
-            };
+            const corpo = document.querySelector('body');
+            corpo.classList = [];
+            if (classes.classList.contains('frio')) corpo.classList.add('frio-bg');
+            if (classes.classList.contains('sol')) corpo.classList.add('sol-bg');
+            if (classes.classList.contains('chuva')) corpo.classList.add('chuva-bg');
+            if (classes.classList.contains('nublado')) corpo.classList.add('nublado-bg');
         });
     }); 
-        
 })()
